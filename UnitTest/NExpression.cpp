7a10c51061de855a091cbf092c2c6c547b1cc8fc
song jun ho <devforgood@gmail.com>
2021-03-26 00:06:44 +0900

    DP N으로 표현

--- UnitTest/NExpression.cpp
+++ UnitTest/NExpression.cpp
@@ -20,79 +20,64 @@ namespace UnitTest
     {
     public:
 
-        int DP(set<int> & input_list, set<int> & output_list, int index, int N, int number)
-        {
-            output_list.clear();
+        int get_basic_number(int N, int cnt) {
+            int res = 0;
 
-            for (set<int>::iterator itr = input_list.begin(); itr != input_list.end(); ++itr)
-            {
-                int a = (*itr) + N;
-                if (a == number)
-                    return true;
-                output_list.insert(a);
-
-                int b = (*itr) - N;
-                if (b == number)
-                    return true;
-                output_list.insert(b);
-
-                int c = (*itr) * N;
-                if (c == number)
-                    return true;
-                output_list.insert(c);
-
-                int d = (*itr) / N;
-                if (d == number)
-                    return true;
-                output_list.insert(d);
-
-                int e = (*itr)*10 + N;
-                if (e == number)
-                    return true;
-                output_list.insert(e);
-
-                int f = N - (*itr);
-                if (f == number)
-                    return true;
-                output_list.insert(f);
-
-                if ((*itr) != 0)
-                {
-                    int g = N / (*itr);
-                    if (g == number)
-                        return true;
-                    output_list.insert(g);
-                }
-
+            while (cnt > 0) {
+                cnt -= 1;
+                res += N * pow(10, cnt);
             }
-            
 
-
-            return false;
+            return res;
         }
 
         int solution(int N, int number) {
-            int answer = 0;
-
-            if (N == number)
+            // 허뎝님의 수정 피드백 -> 테스트 케이스가 바뀌면서 예외 사항을 추가해야 함.
+            if (N == number) {
                 return 1;
-
-            set<int> input_list = { N };
-            set<int> ouput_list;
-            int i;
-            for (i = 2; i < 9; ++i)           
-            {
-                if (DP(input_list, ouput_list, i, N, number))
-                    return i;
-
-                input_list = ouput_list;
             }
 
-            if (i == 9)
-                answer = -1;
-            else
-                answer = i;
+            int answer = -1;
 
+            const int MIN = 8;
+            //1. [ {} x 8 ] 초기화
+            auto s = vector<set<int>>(MIN);
+            // 2. 각 set마다 기본 수 "N" * i 수 초기화
+            int idx = 1;
+
+            for (auto& x : s) {
+                x.insert(get_basic_number(N, idx));
+                idx += 1;
+            }
+
+            //3. n 일반화
+            //   { 
+            //       "n" * i, 
+            //       1번 set 사칙연산 n-1번 set, 
+            //       2번 set 사칙연산 n-2번 set, 
+            //       ...
+            //       n-1번 set 사칙연산 1번 set, 
+            //    } 
+            // number를 가장 최소로 만드는 수 구함.
+            for (int i = 1; i < MIN; i++) {
+                for (int j = 0; j < i; j++) {
+                    for (const auto& op1 : s[j]) {
+                        for (const auto& op2 : s[i - j - 1]) {
+                            s[i].insert(op1 + op2);
+                            s[i].insert(op1 - op2);
+                            s[i].insert(op1 * op2);
+
+                            if (op2 != 0)
+                                s[i].insert(op1 / op2);
+                        }
+                    }
+                }
+
+                if (s[i].find(number) != s[i].end()) {
+                    answer = i + 1;
+                    break;
+                }
+            }
 
             return answer;
         }
