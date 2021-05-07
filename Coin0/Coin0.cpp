c7c8b65e78a16e835e14ae1ef3364071f327f22b
song jun ho <devforgood@gmail.com>
2021-05-07 14:57:15 +0900

    그리디 알고리즘 - 코인 0

---
+++ Coin0/Coin0.cpp
@@ -0,0 +1,32 @@
+﻿
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int N, K;
+    int arr[10] = { 0, };
+    cin >> N >> K;
+    for (int i = 0; i < N; i++)
+        cin >> arr[i];
+
+    int answer = 0;
+
+    for (int i = N - 1; i >= 0; --i)
+    {
+        if (K >= arr[i])
+        {
+            int cnt = K / arr[i];
+            K -= arr[i] * cnt;
+            answer += cnt;
+        }
+        if (K == 0)
+            break;
+    }
+
+
+    cout << answer;
+
+}
