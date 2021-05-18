49effe70ce076e56b1b8a29e70b6b2c2c7a967ea
song junho <devforgood@gmail.com>
2021-05-18 16:11:01 +0900

    Update TravelingSalesmanProblem.cpp
    소스 주석

--- TravelingSalesmanProblem/TravelingSalesmanProblem.cpp
+++ TravelingSalesmanProblem/TravelingSalesmanProblem.cpp
@@ -16,24 +16,30 @@ int dfs(int x, int visited)
 {
 	if (visited == (1 << n) - 1)
 	{
+		// 현재 논드에서 시작 노드로 돌아가기 위한 비용
 		if (arr[x][0] == 0)
-			return 1e9;
+			return 1e9; // 돌아가는길이 없다면 해당 길은 제외되도록한다.
 		else
 			return arr[x][0];
 	}
 
+	// 이전에 방문했던 경로면 과거 데이터를 리턴
 	if (dp[x][visited] != -1)
 		return dp[x][visited];
 
+	// 가장 적은 값을 찾기위해 최대값으로 초기화
 	dp[x][visited] = 1e9;
 	for (int i = 0; i < n; i++)
 	{
+		// 이전 방문 경로 스킵
 		if ((visited & (1 << i)) == (1<<i))
 			continue;
 
+		// 갈수 있는 경로인지 확인
 		if (arr[x][i] == 0)
 			continue;
 
+		// 최단 경로로 재귀호출
 		dp[x][visited] = min(dp[x][visited], dfs(i, visited | (1 << i)) + arr[x][i]);
 	}
 	return dp[x][visited];
