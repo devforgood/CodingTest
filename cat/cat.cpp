889a3045a452b0214ce529638b2c66ae3fbfa576
song jun ho <devforgood@gmail.com>
2021-05-25 01:40:45 +0900

    입출력 - 고양이

---
+++ cat/cat.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "\\    /\\" << endl;
+	cout << " )  ( ')" << endl;
+	cout << "(  /  )" << endl;
+	cout << " \\(__)|" << endl;
+
+}
