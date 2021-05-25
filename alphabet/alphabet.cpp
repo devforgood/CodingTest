13d0f9fef5e877687ed69c129587fd93163636e1
song jun ho <devforgood@gmail.com>
2021-05-25 23:43:12 +0900

    문자열 알파벳

---
+++ alphabet/alphabet.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    cin >> str;
+
+    for (char i = 'a'; i <= 'z'; ++i)
+    {
+        int j = 0;
+        for (; j < str.length(); ++j)
+        {
+            if (str[j] == i)
+            {
+                cout << j;
+                break;
+            }
+        }
+
+        if (j == str.length())
+        {
+            cout << -1;
+        }
+
+        if (i != 'z')
+            cout << " ";
+    }
+}
