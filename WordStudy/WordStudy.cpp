663a8327239a96f7685ca98ee090a86e1d4c07fc
song jun ho <devforgood@gmail.com>
2021-05-26 00:29:17 +0900

    문자열 - 단어 공부

---
+++ WordStudy/WordStudy.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+int main()
+{
+    string str;
+    cin >> str;
+
+    map<char, int> dic;
+
+    int max = 0;
+    char alphabet;
+    int cnt;
+    bool single = false;
+    for (int i = 0; i < str.length(); ++i)
+    {
+        cnt = ++dic[toupper(str[i])];
+
+        if (cnt > max)
+        {
+            single = true;
+            alphabet = toupper(str[i]);
+            max = cnt;
+        }
+        else if (cnt == max)
+        {
+            single = false;
+        }
+    }
+    if (single == false)
+        cout << "?";
+    else
+        cout << alphabet;
+
+}
---
+++ string_repeat/string_repeat.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; ++i)
+    {
+        int cnt;
+        cin >> cnt;
+        string str;
+        cin >> str;
+
+        for (int j = 0; j < str.length(); ++j)
+        {
+            for (int k = 0; k < cnt; ++k)
+            {
+                cout << str[j];
+            }
+        }
+
+        if(i+1 != n)
+            cout << "\n";
+    }
+
+
+}
