2da8e6a081177879e6b2d7bdc9bb20cfc4bac34a
song jun ho <devforgood@gmail.com>
2021-05-27 22:32:30 +0900

    문자열 단어의 개수

---
+++ WordCount/WordCount.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    string str;
+
+    getline(cin, str);
+
+    int cnt = 0;
+
+    if (str.empty() == false)
+    {
+        bool blank = true;
+        for (int i = 0; i < str.length(); ++i)
+        {
+            if (str[i] == ' ' && blank == false)
+            {
+                ++cnt;
+                blank = true;
+            }
+            else
+            {
+                blank = false;
+            }
+        }
+
+        if (str[str.length()-1] != ' ')
+            ++cnt;
+    }
+
+	cout << cnt;
+
+
+}
