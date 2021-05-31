99a4157440579efa6659a71bcf9d6827340ae5fe
song jun ho <devforgood@gmail.com>
2021-05-31 22:52:34 +0900

    기본 수학 - 벌집

---
+++ hive/hive.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	long long n;
+	cin >> n;
+
+	long long dist = 1;
+	if (n > 1)
+	{
+		for (long long i = dist*6+1; i <= 1000000000;)
+		{
+			if (i >= n)
+				break;
+			++dist;
+			i += dist * 6;
+		}
+		++dist;
+	}
+
+	cout << dist;
+}
