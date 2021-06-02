005636e981576c31cb3d0760516bdff187c71820
song jun ho <devforgood@gmail.com>
2021-06-02 22:41:39 +0900

    기본 수학 - 분수 찾기

---
+++ FractionSearch/FractionSearch.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	long long n;
+	cin >> n;
+
+	int numerator = 1;
+	int denominator = 1;
+
+	bool is_denominator = true;
+	for (int i = 1; i < n; ++i)
+	{
+		if (is_denominator)
+		{
+			if (denominator==1)
+			{
+				++numerator;
+				is_denominator = false;
+			}
+			else
+			{
+				++numerator;
+				--denominator;
+			}
+		}
+		else
+		{
+			if (numerator == 1)
+			{
+				++denominator;
+				is_denominator = true;
+			}
+			else
+			{
+				--numerator;
+				++denominator;
+			}
+		}
+	}
+
+	cout << denominator << "/" << numerator;
+}
