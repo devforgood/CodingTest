6dcda02799e48daa7427291d595c855d79cb5b70
song jun ho <devforgood@gmail.com>
2021-06-03 22:38:03 +0900

    기본 수학 - 달팽이는 올라가고 싶다.

---
+++ SnailWantsToGoUp/SnailWantsToGoUp.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int a, b, v;
+	cin >> a >> b >> v;
+
+	int days = (v-a) / (a-b) +1;
+	days += (v - a) % (a - b) > 0 ? 1 : 0;
+	
+
+	cout << days;
+}
