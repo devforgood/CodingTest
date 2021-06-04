22c0655d8224a28e7d969e1e0141dedd75c3ae48
song jun ho <devforgood@gmail.com>
2021-06-04 23:38:33 +0900

    기본 수학 - ACM 호텔

---
+++ ACM_Hotel/ACM_Hotel.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int cnt;
+	cin >> cnt;
+	for (int i = 0; i < cnt; ++i)
+	{
+		int h, n;
+		string w;
+		cin >> h >> w >> n;
+
+		int height = n % h;
+		int num = n / h;
+		if (height == 0)
+			height = h;
+		else
+			++num;
+
+
+		cout << height * 100 + num << endl;
+	}
+}
