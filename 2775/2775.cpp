4e6fa2943193ec44aa4c7a7572a014d699feaf36
song jun ho <devforgood@gmail.com>
2021-06-06 22:57:34 +0900

    기본 수학 - 부녀회장이 될테야, 설탕 배달

---
+++ 2775/2775.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	int arr[15][15];
+	for (int i = 0; i < 15; ++i)
+		arr[0][i] = i;
+
+	int sum = 0;
+	for (int i = 1; i < 15; ++i)
+	{
+		sum = 0;
+		for (int j = 1; j < 15; ++j)
+		{
+			sum += arr[i - 1][j];
+			arr[i][j] = sum;
+		}
+	}
+	
+	int test_case;
+	cin >> test_case;
+	for (int i = 0; i < test_case; ++i)
+	{
+		int k, n;
+		cin >> k >> n;
+
+		cout << arr[k][n] << endl;
+	}
+
+}
---
+++ 2839/2839.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+// 설탕 배달
+int main()
+{
+	int n;
+	cin >> n;
+
+	int cnt = -1;
+	int multiple5 = n / 5;
+
+	while (multiple5 >= 0)
+	{
+		int a = n - (5 * multiple5);
+		if (a == 0)
+		{
+			cnt = multiple5;
+			break;
+		}
+
+		if (a % 3 == 0)
+		{
+			cnt = multiple5 + a / 3;
+			break;
+		}
+
+		--multiple5;
+	}
+
+	cout << cnt;
+
+}
