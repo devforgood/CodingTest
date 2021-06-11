ff670c074db25afee6fcb755b0bc9b4901154964
song jun ho <devforgood@gmail.com>
2021-06-11 23:27:16 +0900

    기본 수학2 - 소인수분해

---
+++ PrimeFactoriztion/PrimeFactoriztion.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>\
+
+using namespace std;
+
+int main()
+{
+	int n, t;
+	cin >> n;
+	t = n;
+	vector<int> factor;
+
+	for (int i = 2; i * i <= t; ++i)
+	{
+		while (n % i == 0)
+		{
+			n /= i;
+			factor.push_back(i);
+		}
+	}
+	if (t != 1)
+	{
+		if (n != 1)
+			factor.push_back(n);
+		for (int i : factor)
+			cout << i << endl;
+	}
+
+}
