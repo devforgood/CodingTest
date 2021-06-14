94f9398808a15f25ffd5e68776850426c6df6076
song jun ho <devforgood@gmail.com>
2021-06-14 22:45:45 +0900

    기본 수학2 - 소수 구하기

---
+++ PrimeFind/PrimeFind.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+using namespace std;
+
+bool PrimeArray[1000001];
+
+void Eratos(int n)
+{
+	PrimeArray[0] = false;
+	PrimeArray[1] = false;
+
+	for (int i = 2; i < n + 1; ++i)
+		PrimeArray[i] = true;
+
+
+	for (int i = 2; i * i <= n; ++i)
+	{
+		if (PrimeArray[i])
+		{
+			for (int j = i * i; j <= n; j += i)
+				PrimeArray[j] = false;
+		}
+	}
+}
+
+int main()
+{
+	int n, m;
+	cin >> m >> n;
+	Eratos(n+1);
+
+	for (int i = m; i <= n; ++i)
+	{
+		if (PrimeArray[i])
+		{
+			cout << i << "\n";
+		}
+	}
+}
+
