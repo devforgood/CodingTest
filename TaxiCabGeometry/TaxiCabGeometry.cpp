b8f881a8fe195e9868256b36a43a53be1197883a
song jun ho <devforgood@gmail.com>
2021-06-15 22:54:14 +0900

    기본 수학 - 택시 기하학 (맨허튼 거리)

---
+++ TaxiCabGeometry/TaxiCabGeometry.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <iomanip>
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+using namespace std;
+
+
+int main()
+{
+
+	double n;
+
+	cin >> n;
+
+
+	cout << fixed;
+	cout.precision(6);
+	cout << n * n * M_PI << "\n";
+	cout << (n * n * 2) << "\n";
+
+}
+
