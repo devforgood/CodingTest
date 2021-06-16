b6067d4ded57b0a84821d754740067c843abb525
song jun ho <devforgood@gmail.com>
2021-06-16 22:28:40 +0900

    기본수학2 - 직각삼각형

---
+++ RightTriangle/RightTriangle.cpp
@@ -0,0 +1,25 @@
+﻿
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h> 
+#include <math.h> 
+#include <algorithm>
+
+int main(void)
+{
+	int arr[3];
+	while (true)
+	{
+		scanf("%d %d %d", &arr[0], &arr[1], &arr[2]);
+
+		if (arr[0] == 0 && arr[1] == 0 && arr[2] == 0)
+			break;
+
+		std::sort(arr, arr + 3);
+
+		if(arr[0] * arr[0] + arr[1] * arr[1] == arr[2] * arr[2])
+			printf("right\n");
+		else
+			printf("wrong\n");
+	}
+}
+
