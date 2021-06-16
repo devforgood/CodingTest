ee2ebf6621b447a1ddd9cfa38348e8f3f76fc60c
song jun ho <devforgood@gmail.com>
2021-06-16 23:10:35 +0900

    기본 수학2 - 직사각형에서 탈출

---
+++ RectangleEscape/RectangleEscape.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x, y, w, h;
+	cin >> x >> y >> w >> h;
+
+	int dist = x;
+	if (dist > w - x)
+		dist = w - x;
+
+	if (dist > y)
+		dist = y;
+
+	if (dist > h - y)
+		dist = h - y;
+	
+
+	cout << dist;
+}
+
