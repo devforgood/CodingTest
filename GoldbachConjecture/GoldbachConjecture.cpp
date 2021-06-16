c89b4c7885ed64ee1f2987256d0a0379e345e185
song jun ho <devforgood@gmail.com>
2021-06-16 23:22:14 +0900

    기본 수학2 - 골드바흐 예상

---
+++ GoldbachConjecture/GoldbachConjecture.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+bool isPrime(int n) 
+{
+    if (n <= 1) {
+        return false;
+    }
+
+    for (int i = 2; i <= sqrt(n); i++) {
+        if ((n % i) == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main()
+{
+	int t, n;
+
+	cin >> t;
+	while (t--)
+	{
+		cin >> n;
+
+		for (int i = n/2; i >0; --i)
+		{
+			if (isPrime(i) && isPrime(n - i))
+			{
+				cout << i << " " << n - i << "\n";
+				break;
+			}
+		}
+	}
+
+}
+
