93e4d5bab78788035b0d35bb7a4928bb979592cc
song jun ho <devforgood@gmail.com>
2021-06-18 23:00:30 +0900

    재귀 - 하노이탑 이동 순서

---
+++ HanoiTower/HanoiTower.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+void move(int from, int to)
+{
+	cout << from << " " << to << '\n';
+}
+
+void hanoi(int n, int from, int by, int to)
+{
+	if (n == 0) return;
+	hanoi(n - 1, from, to, by);
+	move(from, to);
+	hanoi(n - 1, by, from, to);
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	cout << (int)pow(2, n)-1 << '\n';
+	hanoi(n, 1, 2, 3);
+
+}
+
