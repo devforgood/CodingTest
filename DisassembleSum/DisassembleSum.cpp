e0f4aff3e9d62cfdea7264b301d164dc9f42aaff
song jun ho <devforgood@gmail.com>
2021-06-21 23:00:33 +0900

    브루트 포스 - 분해합

---
+++ DisassembleSum/DisassembleSum.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <math.h>
+#include <vector>
+
+using namespace std;
+
+
+
+int main()
+{
+	int n;
+
+	cin >> n;
+
+	int answer = 0;
+	for (int i = 1; i < n; ++i)
+	{
+		int num = i;
+		int disassem = num;
+		int sum = 0;
+		while (disassem)
+		{
+			sum += disassem % 10;
+			disassem /= 10;
+		}
+		if (n == num + sum)
+		{
+			answer = num;
+			break;
+		}
+	}
+
+	cout << answer;
+}
+
