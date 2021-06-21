594cc22af18879de01aef3b8036e521a742ae659
song jun ho <devforgood@gmail.com>
2021-06-21 23:22:10 +0900

    브루트 포스 - 덩치

---
+++ body/body.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <math.h>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	vector<pair<int, int>> vec;
+
+	for (int i = 0; i < n; ++i)
+	{
+		pair<int, int> p;
+		cin >> p.first >> p.second;
+		vec.push_back(p);
+	}
+
+	for (int i = 0; i < n; ++i)
+	{
+		int rank = 1;
+		for (int j = 0; j < n; ++j)
+		{
+			if (i == j)
+				continue;
+
+			if (vec[i].first < vec[j].first && vec[i].second < vec[j].second)
+				++rank;
+		}
+		cout << rank << " ";
+
+	}
+
+
+
+}
+
