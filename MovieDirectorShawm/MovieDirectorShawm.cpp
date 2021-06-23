558a50b219a1855e768f00bb582dd98d036cb574
song jun ho <devforgood@gmail.com>
2021-06-23 22:18:02 +0900

    브루트 포스 - 영화 감독 숌

---
+++ MovieDirectorShawm/MovieDirectorShawm.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <math.h>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+
+int main()
+{
+	int n;
+
+	cin >> n;
+
+	int i = 666;
+	while(true)
+	{
+		if (std::to_string(i).find("666") != std::string::npos)
+			--n;
+
+		if (n == 0)
+		{
+			cout << i;
+			break;
+		}
+		++i;
+	}
+}
+
