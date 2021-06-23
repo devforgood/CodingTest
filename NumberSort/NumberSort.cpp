71cde27918aa696b6f3df7f2478bdfab98489d26
song jun ho <devforgood@gmail.com>
2021-06-23 22:24:22 +0900

    정렬 - 수 정렬하기

---
+++ NumberSort/NumberSort.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+
+int main()
+{
+	int n, v;
+	vector<int> vec;
+	cin >> n;
+
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> v;
+		vec.push_back(v);
+	}
+
+	sort(vec.begin(), vec.end());
+
+	for (int i = 0; i < n; ++i)
+		cout << vec[i] << '\n';
+
+}
+
