e2c3106f83045f35566283cef170df02e1684821
song jun ho <devforgood@gmail.com>
2021-06-24 23:26:54 +0900

    정렬 - 수 정렬하기 3

---
+++ NumberSort3/NumberSort3.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#define _CRT_SECURE_NO_WARNINGS
+#include <algorithm>
+
+using namespace std;
+
+
+
+int main()
+{
+	int n, v;
+	int arr[10000 + 1];
+	scanf("%d", &n);
+	std::fill_n(arr, 10000 + 1, 0);
+
+	int length = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		scanf("%d", &v);
+		++arr[v];
+		if (length < v)
+			length = v;
+	}
+
+	for (int i = 0; i <= length; ++i)
+	{
+		for (int j = 0; j < arr[i]; ++j)
+			printf("%d ", i);
+	}
+	printf("\n");
+
+}
+
