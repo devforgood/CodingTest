96763c255ba96253f88ba65d0793ed90e03ca1a8
song jun ho <devforgood@gmail.com>
2021-06-26 22:43:47 +0900

    정렬 - 좌표 정렬, 정렬인사이트

---
+++ CoordinateSort/CoordinateSort.cpp
@@ -0,0 +1,44 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <map>
+#include <math.h>
+
+
+using namespace std;
+
+
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+
+
+	pair<int, int> point;
+	vector<pair<int, int>> vec;
+	for (int i = 0; i < n; ++i)
+	{
+		scanf("%d %d", &point.first, &point.second);
+		vec.push_back(point);
+	}
+
+
+	sort(vec.begin(), vec.end(), [](const pair<int, int>& a, const pair<int, int>& b) -> bool
+		{
+			if (a.first == b.first)
+			{
+				return a.second < b.second;
+			}
+			return a.first < b.first;
+		});
+
+	for (int i = 0; i < n; ++i)
+	{
+		printf("%d %d\n", vec[i].first, vec[i].second);
+	}
+}
+
---
+++ SortInside/SortInside.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <map>
+#include <math.h>
+
+
+using namespace std;
+
+
+
+int main()
+{
+	string nums;
+	cin >> nums;
+
+	sort(nums.begin(), nums.end(), std::greater<char>());
+
+	cout << nums;
+
+
+}
+
