f53e52e84cab43d4b83af504633221f0984e81e7
song jun ho <devforgood@gmail.com>
2021-06-28 23:04:20 +0900

    정렬 - 좌표 압축

---
+++ AgeSort/AgeSort.cpp
@@ -0,0 +1,49 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <map>
+#include <math.h>
+#include <string>
+#include <tuple>
+
+using namespace std;
+
+
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+
+
+	int age;
+	char str[50 + 1];
+	vector<tuple<int, string, int>> vec;
+	for (int i = 0; i < n; ++i)
+	{
+		scanf("%d %s", &age, str);
+		vec.push_back(make_tuple(age, string(str), i));
+	}
+
+
+	sort(vec.begin(), vec.end(), [](const tuple<int, string, int>& a, const tuple<int, string, int>& b) -> bool
+		{
+			if (std::get<0>(a) == std::get<0>(b))
+			{
+				return std::get<2>(a) < std::get<2>(b);
+			}
+			return std::get<0>(a) < std::get<0>(b);
+		});
+
+	for (int i = 0; i < n; ++i)
+	{
+		if (i != 0 && vec[i] == vec[i - 1])
+			continue;
+
+		printf("%d %s\n", std::get<0>(vec[i]), std::get<1>(vec[i]).c_str());
+	}
+}
+
---
+++ CoordinateCompress/CoordinateCompress.cpp
@@ -0,0 +1,56 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <map>
+#include <math.h>
+#include <set>
+
+
+using namespace std;
+
+
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+
+
+	int x;
+	vector<pair<int, int>> vec;
+
+	for (int i = 0; i < n; ++i)
+	{
+		scanf("%d", &x);
+		vec.push_back(make_pair(x, i));
+	}
+
+	sort(vec.begin(), vec.end(), [](const pair<int, int>& a, const pair<int, int>& b) -> bool
+		{
+			return a.first < b.first;
+		});
+
+	int last_value = 0;
+	int index = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (i != 0 && vec[i].first != last_value)
+			++index;
+		last_value = vec[i].first;
+		vec[i].first = index;
+	}
+
+	sort(vec.begin(), vec.end(), [](const pair<int, int>& a, const pair<int, int>& b) -> bool
+		{
+			return a.second < b.second;
+		});
+
+	for (int i = 0; i < n; ++i)
+	{
+		printf("%d\n", vec[i].first);
+	}
+}
+
