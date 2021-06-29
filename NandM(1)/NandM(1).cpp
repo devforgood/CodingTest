a14b258977bda760820fdb1e93e988a7fe49580d
song jun ho <devforgood@gmail.com>
2021-06-29 22:41:53 +0900

    백트래킹 - N과 M (1)

---
+++ NandM(1)/NandM(1).cpp
@@ -0,0 +1,44 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <map>
+#include <math.h>
+#include <string>
+#include <tuple>
+
+using namespace std;
+
+void backtracking(int n, int m, vector<int> combination, int cur)
+{
+	combination.push_back(cur);
+
+	if (combination.size() == m)
+	{
+		for (int i = 0; i < combination.size(); ++i)
+			printf("%d ", combination[i]);
+		printf("\n");
+		return;
+	}
+
+	for (int i = 1; i <= n; ++i)
+	{
+		if (std::find(combination.begin(), combination.end(), i) != combination.end())
+			continue;
+
+		backtracking(n, m, combination, i);
+	}
+
+}
+
+int main()
+{
+	int n, m;
+	scanf("%d %d", &n, &m);
+	vector<int> combination;
+	for(int i=1;i<=n;++i)
+		backtracking(n, m, combination, i);
+}
+
