5d081453a9a204d03aca621568a8088eee7569ad
song jun ho <devforgood@gmail.com>
2021-06-30 23:37:11 +0900

    백트래킹 - N과 M(3), N과 M(4), N Queen

---
+++ NQueen/NQueen.cpp
@@ -0,0 +1,42 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+
+int n, cnt = 0;
+int temp[16] = { 0, };
+
+void nqueen(int col);
+
+int main(void) {
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        temp[0] = i;
+        nqueen(1);
+    }
+
+    printf("%d\n", cnt);
+
+    return 0;
+}
+
+void nqueen(int col) {
+    if (col == n) {
+        cnt += 1;
+        return;
+    }
+
+    for (int i = 0; i < n; i++) {
+        int check = true;
+        for (int j = 0; j < col; j++) {
+            temp[col] = i;
+            if (temp[j] == temp[col] || ((j - col) == (temp[j] - temp[col])) || ((j - col) == (temp[col] - temp[j]))) {
+                check = false;
+                break;
+            }
+        }
+
+        if (check) {
+            nqueen(col + 1);
+        }
+    }
+}
