3b58618e86fe0c7783a4ea74bdade458e2c11f58
song jun ho <devforgood@gmail.com>
2021-07-01 22:00:13 +0900

    백트래킹 - N Queen

--- NQueen/NQueen.cpp
+++ NQueen/NQueen.cpp
@@ -1,42 +1,50 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <cstdio>
+#include <cmath>
 
 int n, cnt = 0;
-int temp[16] = { 0, };
+int board[16] = { 0, };
 
-void nqueen(int col);
+bool check(int col)
+{
+    for (int i = 0; i < col; ++i)
+    {
+        if (board[col] == board[i] || col - i == abs(board[col] - board[i]))
+            return false;
+    }
+    return true;
+}
 
-int main(void) {
+void nqueen(int col)
+{
+    if (col == n)
+    {
+        ++cnt;
+        return;
+    }
+
+    for (int i = 0; i < n; ++i)
+    {
+        board[col] = i;
+        if (check(col))
+        {
+            nqueen(col + 1);
+        }
+
+    }
+
+
+}
+
+int main(void) 
+{
     scanf("%d", &n);
 
-    for (int i = 0; i < n; i++) {
-        temp[0] = i;
-        nqueen(1);
-    }
+    nqueen(0);
 
     printf("%d\n", cnt);
 
     return 0;
 }
 
-void nqueen(int col) {
-    if (col == n) {
-        cnt += 1;
-        return;
-    }
 
-    for (int i = 0; i < n; i++) {
-        int check = true;
-        for (int j = 0; j < col; j++) {
-            temp[col] = i;
-            if (temp[j] == temp[col] || ((j - col) == (temp[j] - temp[col])) || ((j - col) == (temp[col] - temp[j]))) {
-                check = false;
-                break;
-            }
-        }
-
-        if (check) {
-            nqueen(col + 1);
-        }
-    }
-}
