b7b1687bcc3984f9fdf7976f9bd7440c732d9c31
song jun ho <devforgood@gmail.com>
2021-07-09 23:50:57 +0900

    동적 계획법 1 - 01타일

---
+++ 01Tile/01Tile.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int arr[1000001] = { 0, };
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    int n;
+    cin >> n;
+    arr[1] = 1;
+    arr[2] = 2;
+    for (int i = 3; i <= n; i++) {
+        arr[i] = arr[i - 2] + arr[i - 1];
+        arr[i] %= 15746;
+    }
+   
+    cout << arr[n];
+
+
+}
