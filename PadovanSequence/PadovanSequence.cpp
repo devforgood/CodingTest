a1664e3a2cc498e92863884dc8640a4f30e19fd8
song jun ho <devforgood@gmail.com>
2021-07-10 22:19:31 +0900

    동적 계획법 1 - 파도반 수열

---
+++ PadovanSequence/PadovanSequence.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+long long arr[1000001] = { 0, };
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+
+    arr[1] = 1;
+    arr[2] = 1;
+    arr[3] = 1;
+
+    for (int i = 4; i <= 100; i++) {
+        arr[i] = arr[i - 3] + arr[i - 2];
+    }
+    int n, v;
+    cin >> n;
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> v;
+        cout << arr[v] << "\n";
+    }
+
+
+}
