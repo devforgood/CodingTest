dd29f72b281050bfc94c6bd503a1bfe537b84500
song jun ho <devforgood@gmail.com>
2021-07-16 22:27:39 +0900

    동적 계획법 1 - 포도주 시식

---
+++ WineTest/WineTest.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int arr[10000 + 1];
+int dp[10000 +1];
+
+int main()
+{
+    int n;
+
+    cin >> n;
+
+    for (int i = 1; i <= n; ++i)
+        cin >> arr[i];
+
+
+    dp[1] = arr[1];
+    dp[2] = arr[2] + arr[1];
+
+    for (int i = 3; i <= n; ++i)
+    {
+        dp[i] = max(dp[i - 2], dp[i - 3] + arr[i - 1]) + arr[i];
+        dp[i] = max(dp[i - 1], dp[i]);
+
+    }
+
+    std::cout << dp[n] << "\n";
+}
+
