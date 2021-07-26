f767e1e7c3dc1db85cc73961a7510d81f649583a
song jun ho <devforgood@gmail.com>
2021-07-26 22:16:16 +0900

    동적 계획법 1 - LCS

---
+++ LongestCommonSubsequence/LongestCommonSubsequence.cpp
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string.h>
+
+using namespace std;
+
+
+char s1[1002];
+char s2[1002];
+int dp[1001][1001], i, j;
+
+int main() {
+    scanf("%s %s", s1 + 1, s2 + 1);
+    for (i = 1; s1[i]; i++)
+        for (j = 1; s2[j]; j++)
+            dp[i][j] = max({ dp[i][j - 1], dp[i - 1][j],
+                             dp[i - 1][j - 1] + (s1[i] == s2[j])
+                });
+    printf("%d", dp[i - 1][j - 1]);
+    return 0;
+}
