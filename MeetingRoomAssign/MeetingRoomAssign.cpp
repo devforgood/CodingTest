d948159d5ef07d3092d1d929277b09141bd2a22b
song jun ho <devforgood@gmail.com>
2021-08-05 22:46:48 +0900

    그리디 알고리즘 - 회의실 배정

---
+++ MeetingRoomAssign/MeetingRoomAssign.cpp
@@ -0,0 +1,50 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    int arr[10] = { 0, };
+    cin >> n;
+
+
+	pair<int, int> v;
+	vector<pair<int, int>> vec;
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> v.first >> v.second;
+		vec.push_back(v);
+	}
+
+
+	sort(vec.begin(), vec.end(), [](const pair<int, int>& a, const pair<int, int>& b) -> bool
+		{
+			if (a.second == b.second)
+			{
+				return a.first < b.first;
+			}
+			return a.second < b.second;
+		});
+
+	int answer = 1;
+	int end = vec[0].second;
+	for (int i = 1; i < n; ++i)
+	{
+		if (end <= vec[i].first)
+		{
+			++answer;
+			end = vec[i].second;
+		}
+	}
+
+
+    cout << answer;
+
+}
