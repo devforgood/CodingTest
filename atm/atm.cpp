0abf8ce594fa95816ac0703d3a7b1e5fb73a370b
song jun ho <devforgood@gmail.com>
2021-08-11 22:40:59 +0900

    그리디 알고리즘 - ATM

---
+++ atm/atm.cpp
@@ -0,0 +1,37 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n;
+	int arr[1000] = { 0, };
+	cin >> n;
+
+
+
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> arr[i];
+	}
+
+
+	sort(arr, arr+n);
+
+	int answer = arr[0];
+	for (int i = 1; i < n; ++i)
+	{
+		arr[i] += arr[i - 1];
+		answer += arr[i];
+	}
+
+
+	cout << answer;
+
+}
