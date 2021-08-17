68cd32e5fadaa01227d1ddedbe83c6cca695600b
song jun ho <devforgood@gmail.com>
2021-08-17 22:12:24 +0900

    그리디 알고리즘 - 주유소

---
+++ GasStation/GasStation.cpp
@@ -0,0 +1,44 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+long long value[100000] = { 0, };
+long long length[100000] = { 0, };
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+
+
+
+	for (int i = 0; i < n-1; ++i)
+	{
+		cin >> length[i];
+	}
+
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> value[i];
+	}
+
+	long long answer = 0;
+	long long min_value = 1000000000;
+	for (int i = 0; i < n - 1; ++i)
+	{
+
+		if (min_value > value[i])
+			min_value = value[i];
+
+		answer += length[i] * min_value;
+	}
+
+	cout << answer;
+
+}
