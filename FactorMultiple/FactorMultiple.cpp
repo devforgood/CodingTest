d80a15bdf678227ab0caa1ce02f7fba85fd71e0a
song jun ho <devforgood@gmail.com>
2021-08-18 22:05:37 +0900

    정수론 조합론 - 배수와 약수

---
+++ FactorMultiple/FactorMultiple.cpp
@@ -0,0 +1,38 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int a, b;
+	while(true)
+	{
+		cin >> a >> b;
+		if (a == 0 && b == 0)
+			break;
+
+		if (a % b == 0)
+		{
+			cout << "multiple" << "\n";
+
+		}
+		else if (b % a == 0)
+		{
+			cout << "factor" << "\n";
+		}
+		else
+		{
+			cout << "neither" << "\n";
+		}
+
+	}
+
+
+}
