191099023d64b7dec1b1dd6f635f55153cd1bf3c
song jun ho <devforgood@gmail.com>
2021-08-20 22:22:54 +0900

    정수론 조합론 - 최대공약수와 최소공배수

---
+++ GreatestCommonDenominatorLeastCommonMultiple/GreatestCommonDenominatorLeastCommonMultiple.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+int gcd(int a, int b)
+{
+    int tmp;
+    while (b != 0)
+    {
+        tmp = b;
+        b = a % b;
+        a = tmp;
+    }
+    return a;
+}
+
+int lcm(int a, int b)
+{
+    return (a * b) / gcd(a, b);
+}
+
+
+int main()
+{
+    int a, b;
+    std::cin >> a >> b;
+    std::cout << gcd(a, b) << "\n";
+    std::cout << lcm(a, b) << "\n";
+}
+
