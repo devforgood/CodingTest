3c6035950d4f67fddcd0702ad0052e66056ef161
song jun ho <devforgood@gmail.com>
2021-08-23 23:21:46 +0900

    정수론 조합론 - 검문소

---
+++ Inspection/Inspection.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+
+int GCD(int a, int b)
+{
+    int tmp;
+    while (b != 0)
+    {
+        tmp = b;
+        b = a % b;
+        a = tmp;
+    }
+    return a;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int arr[100] = { 0, };
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + n);
+
+    int gcd = arr[1] - arr[0];
+    for (int i = 2; i < n; ++i)
+        gcd = GCD(gcd, arr[i] - arr[i - 1]);
+
+
+    set<int> answer;
+    for (int i = 2; i*i <= gcd; ++i)
+    {
+        if (gcd % i == 0)
+        {
+            answer.insert(i);
+            answer.insert(gcd / i);
+        }
+    }
+    answer.insert(gcd);
+
+    for (auto v : answer)
+    {
+        cout << v << " ";
+    }
+    cout << "\n";
+}
+
