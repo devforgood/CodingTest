f6ea9fdd37dd71bbfea179356236ce13d62bc0db
song jun ho <devforgood@gmail.com>
2021-08-23 22:09:56 +0900

    정수론 조합론 - 최소공배수

---
+++ LeastCommonMultiple/LeastCommonMultiple.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int gcd(int a, int b)
+{
+    int tmp;
+    while (b != 0)
+    {
+        tmp = b;
+        b = a % b;
+        a = tmp;
+    }
+    return a;
+}
+
+int lcm(int a, int b)
+{
+    return (a * b) / gcd(a, b);
+}
+
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    int a, b;
+    for (int i = 0; i < n; ++i)
+    {
+        std::cin >> a >> b;
+        std::cout << lcm(a, b) << "\n";
+    }
+}
+
