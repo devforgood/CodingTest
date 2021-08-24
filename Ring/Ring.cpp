ee540241d3f77204c5f64cce01000342a86ce60a
song jun ho <devforgood@gmail.com>
2021-08-24 22:43:00 +0900

    정수론 조합론 - 링

---
+++ Ring/Ring.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+
+int GCD(int a, int b)
+{
+    int tmp;
+    while (b != 0)
+    {
+        tmp = b;
+        b = a % b;
+        a = tmp;
+    }
+    return a;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int arr[100] = { 0, };
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> arr[i];
+    }
+
+
+    for (int i = 1; i < n; ++i)
+    {
+        int gcd = GCD(arr[0], arr[i]);
+        cout << arr[0] / gcd << "/" << arr[i] / gcd << "\n";
+    }
+
+   
+}
+
