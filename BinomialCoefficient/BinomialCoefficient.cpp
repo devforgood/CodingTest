6d05170e911b8964d497389409d18030a401d1e7
song jun ho <devforgood@gmail.com>
2021-08-28 23:05:43 +0900

    정수론 조합론 - 이항 계수

---
+++ BinomialCoefficient/BinomialCoefficient.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+
+int factorial(int num)
+{
+    int result = 1;
+    for (int i = 2; i <= num; ++i)
+    {
+        result = result * i;
+    }
+    return result;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, k;
+    cin >> n >> k;
+
+    cout << factorial(n) / (factorial(k) * (factorial(n - k)));
+}
+
