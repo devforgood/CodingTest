1b1a1f1231e83f4b7f443cd7895933d48b9d58db
song jun ho <devforgood@gmail.com>
2021-08-31 22:42:19 +0900

    정수론 조합론 - 다리 놓기

---
+++ BridgeBuild/BridgeBuild.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+
+unsigned long long factorial(int num, int start = 2)
+{
+    long long result = 1;
+    for (int i = start; i <= num; ++i)
+    {
+        result = result * i;
+    }
+    return result;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+
+    for (int i = 0; i < t; ++i)
+    {
+        int n, k;
+        cin >> k >> n;
+
+        int a, b;
+        if (k > n - k)
+        {
+            a = k;
+            b = n - k;
+        }
+        else
+        {
+            a = n - k;
+            b = k;
+        }
+
+
+        cout << factorial(n, a + 1) / (factorial(b)) << "\n";
+    }
+}
+
