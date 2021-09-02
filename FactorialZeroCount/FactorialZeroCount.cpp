4ba2852e61b47398ff46b8c80f31a5ccef4b69a3
song jun ho <devforgood@gmail.com>
2021-09-02 22:35:45 +0900

    정수론 조합론 - 팩토리얼 0의 개수

---
+++ FactorialZeroCount/FactorialZeroCount.cpp
@@ -0,0 +1,25 @@
+﻿// FactorialZeroCount.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+
+    int n;
+    cin >> n;
+
+    int answer = 0;
+    while (n >= 5)
+    {
+        answer += n / 5;
+        n = n / 5;
+    }
+
+    cout << answer;
+}
