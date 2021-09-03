629b41b8955caa9543f5f38cbf972ec190b37b51
song jun ho <devforgood@gmail.com>
2021-09-03 22:48:48 +0900

    정수론 조합론 - 조합 0의 개수

---
+++ CombinationZeroCount/CombinationZeroCount.cpp
@@ -0,0 +1,28 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+
+long long FactorialZeroCount(int n, int divide)
+{
+    long long answer = 0;
+    while (n >= divide)
+    {
+        answer += n / divide;
+        n = n / divide;
+    }
+    return answer;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+
+    int n, m;
+    cin >> n >> m;
+
+    cout << min(FactorialZeroCount(n, 5)-FactorialZeroCount(m, 5)-FactorialZeroCount(n-m, 5), FactorialZeroCount(n, 2) - FactorialZeroCount(m, 2) - FactorialZeroCount(n - m, 2));
+}
