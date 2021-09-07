1496e6f180d475ddf76742d3cfb0377e88e9d2fd
denny.song <denny.song@friendsgames.com>
2021-09-07 12:31:01 +0900

    주석 및 설명 추가

--- 01Tile/01Tile.cpp
+++ 01Tile/01Tile.cpp
@@ -9,6 +9,9 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
 
+    // n의 값에 대응되는 결과를 1부터 차례대로 나열
+    // 수열에 규칙을 찾아 점화식을 만든다. (피보나치 수열과 동일)
+
     int n;
     cin >> n;
     arr[1] = 1;
