f9f32bdecfbca80807667834dae9b13aa6260651
song jun ho <devforgood@gmail.com>
2021-09-09 22:08:59 +0900

    스택 - 괄호

---
+++ bracket/bracket.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+
+bool vps(string bracket)
+{
+    stack<char> st;
+    for (int i = 0; i < bracket.size(); ++i)
+    {
+        if (bracket[i] == ')')
+        {
+            if (st.empty())
+            {
+                return false;
+            }
+            st.pop();
+        }
+        else
+            st.push(bracket[i]);
+    }
+    return st.empty();
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+
+    int n;
+    cin >> n;
+
+    
+    for (int i = 0; i < n; ++i)
+    {
+        string bracket;
+        cin >> bracket;
+        if (vps(bracket))
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+
+}
