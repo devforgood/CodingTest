40e0cc96e692514f1600c56e20155d77e13baa23
song jun ho <devforgood@gmail.com>
2021-09-12 23:35:48 +0900

    분할 정복 - 색종이 만들기

--- ColoredPaperMake/ColoredPaperMake.cpp
+++ ColoredPaperMake/ColoredPaperMake.cpp
@@ -12,16 +12,16 @@ int arr[128][128] = { 0, };
 int white_count = 0;
 int blue_count = 0;
 
-int func(int start_x, int start_y, int end_x, int end_y, int size)
+int func(int start_x, int start_y, int size)
 {
 	if (size == 0)
 		return arr[start_x][start_y];
 
 	int values[4];
-	values[0] = func(start_x, start_y, start_x + size, start_y + size, size / 2);
-	values[1] = func(start_x, start_y + size, start_x + size, end_y, size / 2);
-	values[2] = func(start_x + size, start_y, end_x, start_y + size, size / 2);
-	values[3] = func(start_x + size, start_y + size, end_x, end_y, size / 2);
+	values[0] = func(start_x, start_y, size / 2);
+	values[1] = func(start_x, start_y + size, size / 2);
+	values[2] = func(start_x + size, start_y, size / 2);
+	values[3] = func(start_x + size, start_y + size, size / 2);
 
 	if (values[0] == values[1] && values[0] == values[2] && values[0] == values[3])
 		return values[0];
@@ -56,7 +56,7 @@ int main()
 		}
 	}
 
-	int val = func(0, 0, n, n, n / 2);
+	int val = func(0, 0, n / 2);
 	if(val == 0)
 		++white_count;
 	else if (val == 1)
