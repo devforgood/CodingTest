cbcebade24629b79f0caefed3c05f8ea17dbe7a7
song jun ho <devforgood@gmail.com>
2021-10-19 23:48:11 +0900

    최단 경로 - 특정한 최단 경로

---
+++ particularShortestDistance/particularShortestDistance.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+#include <queue>
+#include <memory.h>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+const int INF = 9e8;
+map<int, vector<pair<int, int> > > edge;
+int dist[20001];
+int N, E;
+
+
+int dijkstar(int start, int end)
+{
+	for (int i = 0; i <= N; ++i)
+		dist[i] = INF;
+
+	priority_queue<pair<int, int>> pq;
+	pq.push({ 0, start });
+	dist[start] = 0;
+
+	while (pq.empty() == false)
+	{
+		int current = pq.top().second;
+		int distance = -pq.top().first;
+		pq.pop();
+
+		if (current == end)
+			break;
+
+		if (dist[current] < distance)
+			continue;
+
+
+		for (auto node : edge[current])
+		{
+			int next = node.first;
+			int nextDistance = node.second + distance;
+
+			if (dist[next] > nextDistance)
+			{
+				dist[next] = nextDistance;
+				pq.push({ -nextDistance, next });
+			}
+		}
+	}
+	return dist[end];
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> N >> E;
+
+
+
+	int a, b, c;
+	for (int i = 0; i < E; ++i)
+	{
+		cin >> a >> b >> c;
+		edge[a].push_back({ b, c });
+		edge[b].push_back({ a, c });
+	}
+
+	int v1, v2;
+	cin >> v1 >> v2;
+
+	int answer = 0;
+	answer += dijkstar(1, v1);
+	answer += dijkstar(v1, v2);
+	answer += dijkstar(v2, N);
+
+	int answer2 = 0;
+	answer2 += dijkstar(1, v2);
+	answer2 += dijkstar(v2, v1);
+	answer2 += dijkstar(v1, N);
+
+	if (answer > answer2)
+		answer = answer2;
+
+	if (answer >= INF || answer < 0)
+		cout << "-1\n";
+	else
+		cout << answer << "\n";
+
+}
