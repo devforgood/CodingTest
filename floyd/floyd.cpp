2a1b5ad5c9f8ecd8b1de87133e08860a44acf386
song jun ho <devforgood@gmail.com>
2021-10-27 22:19:16 +0900

    최단 경로 - 플로이드

---
+++ floyd/floyd.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <queue>
+#include <memory.h>
+#include <map>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+const int INF = 9e8;
+int dist[102][102];
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n, m;
+	cin >> n >> m;
+
+	for (int i = 1; i <= n; ++i)
+		for (int j = 1; j <= n; ++j)
+			dist[i][j] = INF;
+
+	int a, b, c;
+	for (int i = 0; i < m; ++i)
+	{
+		cin >> a >> b >> c;
+		if(dist[a][b] > c)
+			dist[a][b] = c;
+	}
+
+	for (int i = 0; i <= n; ++i)
+		dist[i][i] = 0;
+
+	for (int k = 1; k <= n; ++k)
+	{
+		for (int i = 1; i <= n; ++i)
+		{
+			for (int j = 1; j <= n; ++j)
+			{
+				if (dist[i][j] > dist[i][k] + dist[k][j])
+					dist[i][j] = dist[i][k] + dist[k][j];
+			}
+		}
+	}
+
+	for (int i = 1; i <= n; ++i)
+	{
+		for (int j = 1; j <= n; ++j)
+			if(dist[i][j]==INF)
+				cout << "0 ";
+			else
+				cout << dist[i][j] << " ";
+		cout << "\n";
+	}
+
+}
