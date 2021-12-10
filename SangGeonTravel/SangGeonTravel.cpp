f4be4898c2c2b16b6aeb2beb0b0cc4dbbe8a7499
song jun ho <devforgood@gmail.com>
2021-12-10 23:18:24 +0900

    최소 신장 트리 - 상근이의 여행

---
+++ SangGeonTravel/SangGeonTravel.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <queue>
+#include <memory.h>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n, m;
+		cin >> n >> m;
+
+		for (int i = 0; i < m; ++i)
+		{
+			int a, b;
+			cin >> a >> b;
+
+		}
+
+		cout << n-1 << "\n";
+	}
+}
+
