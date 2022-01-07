4b2eafc2ede5526b3691824ea73daf9489a2e2f0
song jun ho <devforgood@gmail.com>
2022-01-07 22:34:20 +0900

    동적 계획법 3 - 박성원

--- Park Seong-won/Park Seong-won.cpp
+++ Park Seong-won/Park Seong-won.cpp
@@ -10,29 +10,10 @@
 using namespace std;
 
 int n, k;
-int answer = 0;
-string arr[15];
-int dp[15][1 << 15];
-
-void func(int x, int visited, string num)
-{
-	if (visited == (1 << n) - 1)
-	{
-		if (atoi(num.c_str()) % k == 0)
-			++answer;
-
-		return;
-	}
-
-	
-	for (int i = 0; i < n; ++i)
-	{
-		if ((visited & (1 << i)) == (1 << i))
-			continue;
-
-		func(i, visited | (1 << i), num + arr[i]);
-	}
-}
+string str[16];
+int arr[51];
+int arr2[15];
+long long dp[1 << 15][101];
 
 int main()
 {
@@ -43,15 +24,66 @@ int main()
 	cin >> n;
 	for (int i = 0; i < n; ++i)
 	{
-		cin >> arr[i];
+		cin >> str[i];
 	}
 	cin >> k;
 
-	func(0, 0, "");
+	dp[0][0] = 1;
+
+	arr[0] = 1 % k;
+	for (int i = 1; i < 51; ++i)
+		arr[i] = (arr[i - 1] * 10) % k;
+
+	for (int i = 0; i < n; ++i)
+	{
+		int a = 0;
+		for (int j = 0; j < str[i].length(); ++j)
+		{
+			a *= 10;
+			a += str[i][j] - '0';
+			a %= k;
+		}
+		arr2[i] = a;
+	}
 
 
+	for (int cur = 0; cur < (1 << n); ++cur)
+	{
+		for (int i = 0; i < n; ++i)
+		{
+			if ((cur & (1 << i)) == 0)
+			{
+				int next = cur | (1 << i);
+				for (int j = 0; j < k; ++j)
+				{
+					int nextK = ((j * arr[str[i].length()]) % k + arr2[i]) % k;
+					dp[next][nextK] += dp[cur][j];
+				}
+			}
+
+		}
+	}
 
 
+	long long d = 1;
+	long long nu = 0;
 
+	for (int i = 1; i <= n; ++i)
+		d *= i;
+	nu = dp[(1 << n) - 1][0];
+
+	long long b, s;
+	b = max(nu, d);
+	s = min(nu, d);
+
+	while (s != 0)
+	{
+		long long r = b % s;
+		b = s;
+		s = r;
+	}
+
+
+	cout << nu / b << "/" << d / b;
 }
 
