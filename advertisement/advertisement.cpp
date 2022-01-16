f722cfe9ba6e1dcd702dbabd5bfc42f8292ba5cc
song jun ho <devforgood@gmail.com>
2022-01-16 22:24:42 +0900

    문자열 알고리즘 1 - 광고

---
+++ advertisement/advertisement.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+
+	int m;
+	string P;
+	cin >> m >> P;
+
+	vector<int> fail(m);
+	fail[0] = 0;
+
+	for (int i = 1, j = 0; i < m; ++i)
+	{
+		while (j > 0 && P[i] != P[j])
+			j = fail[j - 1];
+
+		if (P[i] == P[j])
+			fail[i] = ++j;
+	}
+
+	cout << m - fail[m - 1] << "\n";
+
+
+
+}
